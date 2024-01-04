046c7b4b1cdf56b7e8d191c0afab9625021f8143
som <somashekarayc@gmail.com>
2024-01-04 22:01:10 +0530

    admin routes and controller functions

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        $notification = array(
+            'message' => 'User Logout Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
+    } // End Method 
+
+    public function Profile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+    }// End Method 
+    
+    public function EditProfile(){
+
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.admin_profile_edit',compact('editData'));
+    }// End Method 
+    
+    public function StoreProfile(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->username = $request->username;
+
+        if ($request->file('profile_image')) {
+           $file = $request->file('profile_image');
+
+           $filename = date('YmdHi').$file->getClientOriginalName();
+           $file->move(public_path('upload/admin_images'),$filename);
+           $data['profile_image'] = $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully', 
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('admin.profile')->with($notification);
+
+    }// End Method
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@ Route::controller(DemoController::class)->group(function () {
     Route::get('/contact', 'ContactMethod')->name('cotact.page');
 });
 
+ // Admin All Route 
+ Route::middleware(['auth'])->group(function () {
+     
+
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin/logout', 'destroy')->name('admin.logout');
+        Route::get('/admin/profile', 'Profile')->name('admin.profile');
+        Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+        Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+    
+        Route::get('/change/password', 'ChangePassword')->name('change.password');
+        Route::post('/update/password', 'UpdatePassword')->name('update.password');
+         
+    });
+    
+    });
